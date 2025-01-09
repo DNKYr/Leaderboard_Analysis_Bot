8866059523de8ba2ddb3706b7feb8ba1dcce6b09
David Li <dtli@lcs.on.ca>
2025-01-09 17:57:23 -0500

    Adding date for accessing data for a specific date in the dataframe.

--- GetHistoricalPrice.py
+++ GetHistoricalPrice.py
@@ -1,5 +1,10 @@
+from datetime import datetime
 import yfinance as yf
 import pandas as pd
+import numpy as np
+
+def datetime_to_datetime64ns(dt_obj):
+  return np.datetime64(dt_obj)
 
 stocks = set()
 
@@ -7,8 +12,13 @@ df = pd.read_csv("Test.csv")
 
 for i in df["Symbol"]:
     stocks.add(i)
+historical_stock_price = yf.download(stocks, start="2024-01-01", end="2025-01-07")
 
-data = yf.download(stocks, start="2024-01-01", end="2025-01-07")
-print(data[0])
+date_str = df["Date"][0]
+date_object = datetime.strptime(date_str, "%m/%d/%Y").date()
+date_object = date_object.strftime("%Y-%m-%d")
+
+print(historical_stock_price.loc[date_object])
+
 
 
